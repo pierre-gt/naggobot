045f4849f1fcce281387f1970764c388a74d4940
NaggoBot <naggobot@vps-d0105536.vps.ovh.net>
2021-12-27 21:08:50 +0000

    ajustement de l'archivage

--- remove_obsolete_sections.py
+++ remove_obsolete_sections.py
@@ -5,15 +5,15 @@ import pywikibot
 def remove_obsolete_sections(wikicode, commons):
     aSauver = False
     archive = ""
-    for section in wikicode.get_sections(levels=[2], matches="suppression sur Commons"):
+    for section in wikicode.get_sections(levels=[2], matches="suppression sur Commons", include_lead=False):
+        print("Section :", section)
         templates = section.filter_templates()
         for template in templates:
             nocat = True
             if template.has('nocat'):
                 if template.name == "Fichier proposé à la suppression sur Commons":
                     aSauver = True
-                    archive += str(section)
-                    section.replace(section, "")
+                    archive = replace_section(archive, section)
                     print("section 1" + str(section))
                 break
             if template.name == "Fichier proposé à la suppression sur Commons":
@@ -22,8 +22,7 @@ def remove_obsolete_sections(wikicode, commons):
                 fichierCommons = pywikibot.Page(commons, chemin)
                 if not fichierCommons.exists():
                     aSauver = True
-                    archive += str(section)
-                    section.replace(section, "")
+                    archive = replace_section(archive, section)
                     print("section 2" + str(section))
                     print(chemin, " n'existe plus : on retire la section")
                 else:
@@ -37,10 +36,42 @@ def remove_obsolete_sections(wikicode, commons):
                                 "L'image {%s} n'est plus dans les catégories de suppression : retrait" % chemin)
                 if nocat:
                     aSauver = True
-                    archive += str(section)
-                    section.replace(section, "")
+                    archive = replace_section(archive, section)
                     print("section 3" + str(section))
     return aSauver, archive
+
+def replace_section(archive, section):
+    lines_archive = []
+    lines_conserver = []
+    conserver = False
+    for line in str(section).split("\n"):
+        if line.startswith("{{") and not line.startswith("{{Fichier proposé") and not line.startswith("{{RI"):
+            conserver = True
+        if line.startswith("==") and "suppression sur Commons" in line:
+            conserver = False
+        if conserver:
+            lines_conserver += [line]
+        else:
+            lines_archive += [line]
+    if len(lines_conserver)>0:
+        lines_conserver += ['']
+        if lines_conserver[-2] != '':
+            lines_conserver += ['']
+        if lines_conserver[-3] != '':
+            lines_conserver += ['']
+    if len(lines_archive)>0:
+        lines_archive += ['']
+        if lines_archive[-2] != '':
+            lines_archive += ['']
+        if lines_archive[-3] != '':
+            lines_archive += ['']
+    print(lines_conserver)
+    print(lines_archive)
+    archive += "\n".join(lines_archive)
+    print("nouvelle section", "\n".join(lines_conserver),"fin nouvelle section")
+    section.replace(section, "\n".join(lines_conserver))
+#    section.replace(section, "x\n")
+    return archive
 
 def archive_commons(site, talkPage, texte):
     page_archive = pywikibot.Page(site, talkPage.title()+"/Archive Commons")
